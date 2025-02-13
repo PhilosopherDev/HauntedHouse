39a8d86766b05c02e2edf2771f0a73b2e1181840
BYUNGMIN CHOI <nekarakube@gmail.com>
2025-02-13 23:50:02 +0900

    Fog - THREE 내장 안개로 Fog, FogExp2 가 있다. 각각 거리와 밀도로 안개를 조정한다. color 꿀팁 - style에 아무 색상이나 지정하고 거기서 스포이트 처럼 현재 색상을 찾을 수 있다. #03343f 로 배경색을 직접 찾아서 적용해줬다.

--- src/script.js
+++ src/script.js
@@ -425,6 +425,11 @@ sky.material.uniforms['mieCoefficient'].value = 0.1;
 sky.material.uniforms['mieDirectionalG'].value = 0.95;
 sky.material.uniforms['sunPosition'].value.set(0.3, -0.038, -0.95);
 
+/**
+ * Fog
+ */
+// scene.fog = new THREE.Fog('#ff0000', 10, 13); // near/far - 거리로 조절
+scene.fog = new THREE.FogExp2('#03343f', 0.1); // density - 밀도로 조절
 
 /**
  * Animate
